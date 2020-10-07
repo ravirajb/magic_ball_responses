c91e101fc1f9c3a7e3cc2cc567b43e6c558560c1
Ravi Bitragunta <messagerrb@gmail.com>
2020-10-07 22:03:15 +0530

    First commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() => runApp(
       MaterialApp(
-        home: null,
+        home: Scaffold(
+          backgroundColor: Colors.red,
+          appBar: AppBar(
+            title: Center(child: Text('Dicee')),
+            backgroundColor: Colors.red,
+          ),
+          body: BallPage(),
+        ),
       ),
     );
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  int ballNumber = 1;
+
+  void generateDiceNumbers() {
+    ballNumber = Random().nextInt(5) + 1;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  generateDiceNumbers();
+                  //leftDiceNumber = 5;
+                });
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
